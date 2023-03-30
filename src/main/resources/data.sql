df1716f22aea11d4d90aa518b0ffa5592129bb88
verybadgag <memel_esmel@hotmail.fr>
2023-03-30 17:23:27 +0200

    init

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,32 +1,36 @@
 USE spring_db;
 
 CREATE TABLE IF NOT EXISTS Commandes (
-  id_commande INT PRIMARY KEY AUTO_INCREMENT,
-  nom VARCHAR(50)
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nom VARCHAR(50),
+  date_commande DATE,
+  montant_total DECIMAL(10,2)
 );
-INSERT INTO commande (id, date_commande, montant_total) VALUES (1, '2022-01-01', 150.0);
-INSERT INTO commande (id, date_commande) VALUES (2, '2022-02-15');
-INSERT INTO commande (date_commande, montant_total) VALUES ('2022-03-30', 200.0);
 
-CREATE TABLE IF NOT EXISTS client (
-  id_type_client INT PRIMARY KEY AUTO_INCREMENT,
-  nom VARCHAR(50)
+INSERT INTO Commandes (id, date_commande, montant_total) VALUES (1, '2022-01-01', 150.0);
+INSERT INTO Commandes (id, date_commande, montant_total) VALUES (2, '2022-02-15', 456.0);
+INSERT INTO Commandes (id, date_commande, montant_total) VALUES (3, '2022-03-30', 200.0);
+
+CREATE TABLE IF NOT EXISTS clients (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nom VARCHAR(50),
+  prenom VARCHAR(50),
+  email VARCHAR(100),
+  telephone VARCHAR(20)
 );
-INSERT INTO client (nom, prenom, email, telephone) VALUES ('Doe', 'John', 'johndoe@example.com', '555-1234');
-INSERT INTO client (id, nom, prenom, email, telephone) VALUES (1, 'Doe', 'John', 'johndoe@example.com', '555-1234');
-INSERT INTO client (id, nom, prenom, email, telephone) VALUES (2, 'Smith', 'Jane', 'janesmith@example.com', '555-5678');
+
+INSERT INTO clients (id, nom, prenom, email, telephone) VALUES (1, 'Doe', 'John', 'johndoe@example.com', '0754566583');
+INSERT INTO clients (id, nom, prenom, email, telephone) VALUES (2, 'Poe', 'Mohn', 'poemohn@example.com', '0785653658');
+INSERT INTO clients (id, nom, prenom, email, telephone) VALUES (3, 'Smith', 'Jane', 'janesmith@example.com', '0756256589');
 
 CREATE TABLE IF NOT EXISTS type_client (
   id_type_client INT PRIMARY KEY AUTO_INCREMENT,
-  nom VARCHAR(50)
+  nom VARCHAR(50),
+  description VARCHAR(100)
 );
-INSERT IGNORE INTO client (nom) VALUES (1, 'PARTICULIER', 'Catégorie des particuliers');
-INSERT IGNORE INTO client (nom) VALUES (2, 'PROFESSIONNEL', 'Catégorie des pros');
 
-
-
-
-
+INSERT IGNORE INTO type_client (id_type_client, nom, description) VALUES (1, 'PARTICULIER', 'Catégorie des particuliers');
+INSERT IGNORE INTO type_client (id_type_client, nom, description) VALUES (2, 'PROFESSIONNEL', 'Catégorie des pros');
 
 --ALTER TABLE type_client MODIFY COLUMN id_type_client INT AUTO_INCREMENT;
---ALTER TABLE type_personne MODIFY COLUMN id_typepersonne INT AUTO_INCREMENT;
+
